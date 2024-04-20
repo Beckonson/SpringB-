5ca6ab036cd943465c95328deff2542fae9614e8
Becckonson <multititech11@gamil.com>
2024-04-20 11:18:11 +0100

    Sting project

---
+++ src/main/java/com/Besto/running/WellcomeSpring.java
@@ -0,0 +1,11 @@
+package com.Besto.running;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class WellcomeSpring {
+    public String getWellcomeSpring(){
+        return "Iam in the spring";
+    }
+}
